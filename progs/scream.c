55f62d3467d1f712fb277e1961f07ec90c7bda45
randomuser <randomuser@tilde.club>
2021-02-16 10:38:21 -0600

    retab scream.c

--- progs/scream.c
+++ progs/scream.c
@@ -5,30 +5,30 @@
  * good night and good riddance */
 
 char newchar(char c, int i) {
-	switch(i) {
-		case 0:
-			if(isupper(c)) return c + 32;
-			if(c == 33) return -2;
-			else return c;
-		case 1:
-			if(islower(c)) return c - 32;
-			if(c == 33) return -2;
-			else return c;
-		default: return -1;
-	}
+  switch(i) {
+    case 0:
+      if(isupper(c)) return c + 32;
+      if(c == 33) return -2;
+      else return c;
+    case 1:
+      if(islower(c)) return c - 32;
+      if(c == 33) return -2;
+      else return c;
+    default: return -1;
+  }
 }
 int advi(int i) {
-	switch(i) {
-		case 0: return 1;
-		case 1: return 0;
-		default: return -1;
-	}
+  switch(i) {
+    case 0: return 1;
+    case 1: return 0;
+    default: return -1;
+  }
 }
 int main(void) {
-	char c;
-	int i = 0;
-	while((c = getchar()) != EOF) {
-		putchar(newchar(c, i));
-		i = advi(i);
-	}
+  char c;
+  int i = 0;
+  while((c = getchar()) != EOF) {
+    putchar(newchar(c, i));
+    i = advi(i);
+  }
 }
