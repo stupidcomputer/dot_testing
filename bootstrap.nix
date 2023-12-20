48d9230d23febf9308cf1cfac906b129b6e72723
randomuser <rndusr@beepboop.systems>
2023-12-20 15:19:01 -0600

    changes

---
+++ bootstrap.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "rndusr's nixos flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    "virtbox" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./bootstrap.nix
+	./boxes/virtbox.nix
+      ];
+    };
+  };
+}
