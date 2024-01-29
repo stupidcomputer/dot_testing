a261bccd0a5fd6408e222900efd7e8324d94cbfc
randomuser <randomuser@tilde.club>
2024-01-28 19:02:49 -0600

    some changes to mlg and global computers

--- boxes/mlg/nvidia.nix
+++ boxes/mlg/nvidia.nix
@@ -30,10 +30,7 @@
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
+      sync.enable = true;
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
