6094c5f5dd44fb1890ad080082f54c55b0ec0b98
randomuser <randomuser@tilde.club>
2024-03-14 07:45:25 -0500

    split color configuration into seperate file

--- config.h
+++ config.h
@@ -91,35 +91,7 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"#161510",
-	"#a32810",
-	"#727a18",
-	"#a37720",
-	"#3d6266",
-	"#7a4955",
-	"#557a55",
-	"#8e8463",
-
-	/* 8 bright colors */
-	"#4c4635",
-	"#cc3214",
-	"#8e991e",
-	"#cc9528",
-	"#4c7b7f",
-	"#995b6b",
-	"#6b996b",
-	"#ccbc8e",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-};
-
+#include <colors.h>
 
 /*
  * Default colors (colorname index)
