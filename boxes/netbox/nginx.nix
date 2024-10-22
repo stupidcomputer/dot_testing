7a488636f4cc63d0977dfbee37770aa218d653f4
rndusr <ryan@beepboop.systems>
2024-10-22 08:22:06 -0500

    add gitea and nginx

--- boxes/netbox/nginx.nix
+++ boxes/netbox/nginx.nix
@@ -16,6 +16,18 @@
     };
   };
 
+  services.nginx.virtualHosts."tfb.beepboop.systems" = {
+    forceSSL = true;
+    enableACME = true;
+    root = "/var/www/tfb.beepboop.systems";
+    locations."/" = {
+      extraConfig = ''
+        port_in_redirect off;
+        absolute_redirect off;
+      '';
+    };
+  };
+
   security.acme = {
     acceptTerms = true;
     defaults.email = "nickforanick@protonmail.com";
