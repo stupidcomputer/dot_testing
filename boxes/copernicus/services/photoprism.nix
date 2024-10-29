672f385a2a9e8d6e58d64c8eada786e578388dc4
stupidcomputer <ryan@beepboop.systems>
2024-10-28 20:54:17 -0500

    add the initial password thing

--- boxes/copernicus/services/photoprism.nix
+++ boxes/copernicus/services/photoprism.nix
@@ -4,10 +4,10 @@
   services.photoprism = {
     enable = true;
     originalsPath = "/var/lib/photoprism/originals";
-    passwordFile = "/home/usr/wg-keys/photoprism-password";
     address = "10.100.0.2";
     settings = {
       PHOTOPRISM_ADMIN_USER = "usr";
+      PHOTOPRISM_ADMIN_PASSWORD = "testing"; # THIS IS AN INITIAL PASSWORD -- changed later
       PHOTOPRISM_SITE_TITLE = "photos.beepboop.systems";
       PHOTOPRISM_SITE_URL = "https://photos.beepboop.systems";
       PHOTOPRISM_DEFAULT_LOCALE = "en";
