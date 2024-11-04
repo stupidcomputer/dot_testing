66424aa9b1cdb400aa9bf511e34c9d0cd89dc487
stupidcomputer <ryan@beepboop.systems>
2024-11-04 02:33:03 -0600

    whoops

--- home/rbw/default.nix
+++ home/rbw/default.nix
@@ -1,6 +1,10 @@
 { lib, config, pkgs, home, ... }:
 
 {
+  home.packages = with pkgs; [
+    rbw
+  ];
+
   home.file = {
     ".config/rbw/config.json" = {
       source = ../../.config/rbw/config.json;
