3edb48896324729d32bdc7f84864881627176992
stupidcomputer <ryan@beepboop.systems>
2024-11-04 00:50:54 -0600

    move rbw from hm config to .config for phone

--- home/rbw/default.nix
+++ home/rbw/default.nix
@@ -1,12 +1,9 @@
 { lib, config, pkgs, home, ... }:
 
 {
-  programs.rbw = {
-    enable = true;
-    settings = {
-      base_url = "https://bitwarden.beepboop.systems";
-      email = "bit@beepboop.systems";
-      pinentry = pkgs.pinentry-gnome3;
+  home.file = {
+    ".config/rbw/config.json" = {
+      source = ../../.config/rbw/config.json;
     };
   };
 }
