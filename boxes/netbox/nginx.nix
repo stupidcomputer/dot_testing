fe6b4556a43d7a2957b200258bd752c6b503e1a9
stupidcomputer <ryan@beepboop.systems>
2025-02-19 15:55:17 -0600

    make beepboop.systems proxy for github pages

--- boxes/netbox/nginx.nix
+++ boxes/netbox/nginx.nix
@@ -3,17 +3,28 @@
     enable = true;
     clientMaxBodySize = "100m";
     defaultSSLListenPort = 442;
+    appendHttpConfig = ''
+      proxy_cache_path /tmp/proxy_cache levels=1:2 keys_zone=cachecache:100m max_size=1g inactive=365d use_temp_path=off;
+      map $status $cache_header {
+        200     "public";
+        302     "public";
+        default "no-cache";
+      }
+
+      error_log stderr;
+      access_log syslog:server=unix:/dev/log combined;
+    '';
 
     virtualHosts = {
       "beepboop.systems" = {
         forceSSL = true;
         enableACME = true;
-        root = "/var/www/beepboop.systems";
         locations."/" = {
           extraConfig = ''
             port_in_redirect off;
             absolute_redirect off;
           '';
+          proxyPass = "https://stupidcomputer.github.io/stupidcomputer/";
         };
       };
 
