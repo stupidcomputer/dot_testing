83d22f26b2b28a0a35a7c99f23c5ec5c8b751ac3
stupidcomputer <ryan@beepboop.systems>
2025-10-11 12:36:07 -0500

    inc flasktrack

--- builds/flasktrack.nix
+++ builds/flasktrack.nix
@@ -3,7 +3,7 @@ with python3Packages;
 let
   repo = fetchgit {
     url = "https://github.com/stupidcomputer/flasktrack";
-    hash = "sha256-qa1VqlRlmayZ13j21/jwH7m4OFsu9tdb2AvCkcBXG8w=";
+    hash = "sha256-KzX5BhFcrv+LUxYnkiQg5ooJLKa08etJ5x2aUqy2d5E=";
   };
 in buildPythonApplication rec {
   pname = "flasktrack";
