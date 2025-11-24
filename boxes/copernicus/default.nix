8a7d419ecd1386ad4cd85ffc915003324722b5b7
stupidcomputer <ryan@beepboop.systems>
2025-11-24 14:40:30 -0600

    some changes to copernicus

--- boxes/copernicus/default.nix
+++ boxes/copernicus/default.nix
@@ -27,6 +27,13 @@
     ../../config/zathura
   ];
 
+  nix.settings = {
+    cores = 16;
+    max-jobs = 24;
+    download-buffer-size = 10000000000;
+    warn-dirty = false;
+  };
+
   services.tailscale = {
     enable = true;
   };
@@ -45,11 +52,21 @@
     i3-swallow
   ];
 
-  services.ollama.enable = true;
-  services.open-webui.enable = true;
+  nixpkgs.config.cudaSupport = true;
 
   fonts.packages = with pkgs; [
     fantasque-sans-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    corefonts
+    vistafonts
+    proggyfonts
   ];
 
   virtualisation.virtualbox.host.enable = true;
