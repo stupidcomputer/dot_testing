06de67afbb97310eed3f8e9c904ad0e0c565ea75
stupidcomputer <ryan@beepboop.systems>
2026-04-18 15:04:59 -0500

    make ssh on theseus available over intnet

--- boxes/theseus/ssh.nix
+++ boxes/theseus/ssh.nix
@@ -2,13 +2,27 @@
 {
   services.openssh = {
     enable = true;
-    ports = [55555];
+    listenAddresses = [
+      {
+        addr = "127.0.0.1";
+        port = 55555;
+      }
+      {
+        addr = "10.100.0.1";
+        port = 22;
+      }
+    ];
     settings = {
       X11Forwarding = false;
       AllowTcpForwarding = true;
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
+  };
+
+  systemd.services.sshd = {
+    requires = [ "wg-quick-wg0.service" ];
+    after = [ "wg-quick-wg0.service" ];
   };
 
   users.users.usr.openssh.authorizedKeys.keys = [
