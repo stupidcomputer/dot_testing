825f5e212a4a3837fd91879c8f2c462bfbc02e4a
stupidcomputer <ryan@beepboop.systems>
2026-07-17 11:11:51 -0500

    upgrade to 26.05

--- flake.nix
+++ flake.nix
@@ -15,11 +15,11 @@
       url = "git+ssh://git@github.com/stupidcomputer/flagman.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs";
     agenix.url = "github:ryantm/agenix";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     firefox-addons = {
